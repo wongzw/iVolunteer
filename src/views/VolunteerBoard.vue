d344cecb904a66704c49e63d9aaede0f9dea2ba3
Zhou Wai <wongzhouwai@gmail.com>
2022-10-25 16:50:00 +0800

    Update Footer

--- src/views/VolunteerBoard.vue
+++ src/views/VolunteerBoard.vue
@@ -1,14 +1,12 @@
 <template>
   <VolOnboard />
-  <TheFooter />
 </template>
 
 <script>
 import VolOnboard from "../components/onboarding/VolOnboard.vue";
-import TheFooter from "@/components/TheFooter.vue";
 
 export default {
   name: "VolunteerBoard",
-  components: { VolOnboard, TheFooter },
+  components: { VolOnboard },
 };
 </script>
