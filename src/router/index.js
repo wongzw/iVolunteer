2d4b4d17c39307763acd495b3fd6b2173c1afb6b
Zhou Wai <wongzhouwai@gmail.com>
2022-10-25 16:10:30 +0800

    Update Register Routes

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import UserDashboardView from "@/views/UserDashboardView.vue";
 import VolunteerBoard from "@/views/VolunteerBoard.vue";
 import OrgBoard from "@/views/OrgBoard.vue";
 import RewardsRedemption from "@/views/RewardsRedemption.vue";
+import RegisterShell from "@/views/RegisterShell.vue";
 import store from "@/store";
 
 const routes = [
@@ -29,14 +30,21 @@ const routes = [
     },
   },
   {
-    path: "/volunteer/register",
-    name: "VolunteerRegister",
-    component: VolunteerRegister,
-  },
-  {
-    path: "/organisation/register",
-    name: "OrgRegister",
-    component: OrgRegister,
+    path: "/register",
+    name: "RegisterShell",
+    component: RegisterShell,
+    children: [
+      {
+        path: "volunteer",
+        name: "VolunteerRegister",
+        component: VolunteerRegister,
+      },
+      {
+        path: "organisation",
+        name: "OrgRegister",
+        component: OrgRegister,
+      },
+    ],
   },
   {
     path: "/notifications",
