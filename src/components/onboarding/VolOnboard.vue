ca38e2a213c084ffc5d81442c82a1c7c6bd6f0e5
a.anghs22 <a.anghs22@gmail.com>
2022-10-26 15:36:36 +0800

    push to profile page instead upon onboarding

--- src/components/onboarding/VolOnboard.vue
+++ src/components/onboarding/VolOnboard.vue
@@ -93,7 +93,7 @@ export default {
         alert("Please fill in all fields!");
       } else {
         this.updateDb(this.$store.state.id);
-        this.$router.push({ path: "/volunteer", replace: true });
+        this.$router.push({ path: "/volunteer/profile", replace: true });
       }
     },
     async updateDb(uid) {
