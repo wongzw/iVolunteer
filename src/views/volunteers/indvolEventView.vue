8c7241b39862340c278024db3977aa71ed3fc96a
Zhou Wai <wongzhouwai@gmail.com>
2022-10-28 11:41:33 +0800

    update volunteer file organsation

--- src/views/volunteers/indvolEventView.vue
+++ src/views/volunteers/indvolEventView.vue
@@ -5,7 +5,7 @@
   <script>
 import { db } from "../../firebase.js";
 import { getDoc, doc } from "firebase/firestore";
-import IndividualEvent from "../../components/IndividualEvent.vue";
+import IndividualEvent from "../../components/volunteer/IndividualEvent.vue";
 
 export default {
   name: "indEventView",
