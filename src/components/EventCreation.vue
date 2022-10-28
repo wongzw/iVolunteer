7edf903b1d0562707ec0b1aa3a92f55a077e8673
Zhou Wai <wongzhouwai@gmail.com>
2022-10-28 10:23:59 +0800

    Replace individual event alert

--- src/components/EventCreation.vue
+++ src/components/EventCreation.vue
@@ -20,8 +20,9 @@
         ></a-input>
         <label class="eventCreation">Event Photo</label><br />
         <div class="eventCreation">
-          <input type="file" name="file" @change="previewFile">
-        </div><br>
+          <input type="file" name="file" @change="previewFile" />
+        </div>
+        <br />
         <label class="eventCreationLeft">Event Type</label><br /><br />
         <a-select
           v-model:value="eventType"
@@ -36,16 +37,32 @@
           <a-select-option value="Clean-up">Clean-up</a-select-option>
         </a-select>
         <label class="eventCreation">Event Description</label><br />
-        <a-textarea style="width: 60%; margin-bottom: 10px" v-model:value="eventDescription" maxlength="1000" placeholder="Enter the description of your event (Max char: 1000)" :rows="6" />
+        <a-textarea
+          style="width: 60%; margin-bottom: 10px"
+          v-model:value="eventDescription"
+          maxlength="1000"
+          placeholder="Enter the description of your event (Max char: 1000)"
+          :rows="6"
+        />
         <label class="eventCreation">Event Details</label><br />
         <label class="eventCreation">Date</label><br />
-        <a-space style="width: 60%; margin-bottom: 10px" direction="vertical" :size="12">
-          <a-range-picker v-model:value="eventDate" style="width: 100%; margin-bottom: 10px" />
+        <a-space
+          style="width: 60%; margin-bottom: 10px"
+          direction="vertical"
+          :size="12"
+        >
+          <a-range-picker
+            v-model:value="eventDate"
+            style="width: 100%; margin-bottom: 10px"
+          />
         </a-space>
         <label class="eventCreation">Time</label><br />
         <a-space style="width: 60%; margin-bottom: 10px" direction="vertical">
-          <a-time-range-picker v-model:value="eventTime" style="width: 100%; margin-bottom: 10px" />
-        </a-space><br />
+          <a-time-range-picker
+            v-model:value="eventTime"
+            style="width: 100%; margin-bottom: 10px"
+          /> </a-space
+        ><br />
         <label class="eventCreationLeft">Location</label>
         <label class="eventCreationRight">Number of openings</label><br />
         <a-input
@@ -56,13 +73,20 @@
           placeholder="Location"
         ></a-input>
         <a-input
-          style="width: 30%; margin-bottom: 10px; margin-right: 20%; float: right"
+          style="
+            width: 30%;
+            margin-bottom: 10px;
+            margin-right: 20%;
+            float: right;
+          "
           class="input"
           type="number"
           v-model:value="noOfOpenings"
           placeholder="Enter a number"
-        ></a-input><br /><br/>
-        <label class="eventCreationLeft">Event causes (Max: 3)</label><br /><br />
+        ></a-input
+        ><br /><br />
+        <label class="eventCreationLeft">Event causes (Max: 3)</label
+        ><br /><br />
         <a-select
           v-model:value="eventCauses"
           mode="tags"
@@ -79,7 +103,6 @@
           placeholder="Press tab to add another badge"
         ></a-select>
 
-      
         <div id="ant-button">
           <a-button
             htmlType="submit"
@@ -97,8 +120,11 @@
 
 <script>
 import { collection, addDoc, doc, getDoc } from "firebase/firestore";
-import { getStorage, uploadBytes, ref, getDownloadURL } from 'firebase/storage'
+import { getStorage, uploadBytes, ref, getDownloadURL } from "firebase/storage";
 import { db } from "../firebase.js";
+import { notification } from "ant-design-vue";
+import { SmileOutlined } from "@ant-design/icons-vue";
+import { h } from "vue";
 
 export default {
   name: "EventCreation",
@@ -119,64 +145,80 @@ export default {
       badgeAwarded: [],
       participants: {},
       orgId: "",
-      orgName: ""
-    }
+      orgName: "",
+    };
   },
+
+  setup() {
+    const eventCreateNotification = () => {
+      notification.open({
+        message: "Success",
+        description: "Event successfully created!",
+        duration: 3,
+        icon: () => h(SmileOutlined, { style: "color: #020957" }),
+      });
+    };
+
+    return eventCreateNotification;
+  },
+
   methods: {
     async createDb() {
-        const storage = getStorage()
-        const storageRef = ref(storage, "Event photos/" + this.file.name);
-        uploadBytes(storageRef, this.file).then((snapshot) => {
-          getDownloadURL(snapshot.ref).then((downloadURL) => {
-            this.eventUrl = downloadURL;
-            const colRef = collection(db, 'events')
-            const docRef = addDoc(colRef, {
-              eventName: this.eventName,
-              location: this.location,
-              eventUrl: this.eventUrl,
-              eventType: this.eventType,
-              eventDescription: this.eventDescription,
-              dateStart: this.eventDate[0].format('DD-MM-YYYY'),
-              dateEnd: this.eventDate[1].format('DD-MM-YYYY'),
-              timeStart: this.eventTime[0].format("HH:mm:ss"),
-              timeEnd: this.eventTime[1].format("HH:mm:ss"),
-              duration: Number((this.eventDate[1].diff(this.eventDate[0], 'days') + 1) * this.eventTime[1].diff(this.eventTime[0], 'hours')),
-              noOfOpenings: Number(this.noOfOpenings),
-              eventCauses: this.eventCauses,
-              badgeAwarded: this.badgeAwarded,
-              participants: {},
-              orgId: this.$store.state.id,
-              orgName: this.$store.state.details.orgName
-            }).then(() => {
-                  alert("Event successfully created!")
-                  console.log("Document successfully written!");
-                }).catch((error) => {
-                  console.error("Error writing document: ", error);
-                });
+      const storage = getStorage();
+      const storageRef = ref(storage, "Event photos/" + this.file.name);
+      uploadBytes(storageRef, this.file).then((snapshot) => {
+        getDownloadURL(snapshot.ref).then((downloadURL) => {
+          this.eventUrl = downloadURL;
+          const colRef = collection(db, "events");
+          const docRef = addDoc(colRef, {
+            eventName: this.eventName,
+            location: this.location,
+            eventUrl: this.eventUrl,
+            eventType: this.eventType,
+            eventDescription: this.eventDescription,
+            dateStart: this.eventDate[0].format("DD-MM-YYYY"),
+            dateEnd: this.eventDate[1].format("DD-MM-YYYY"),
+            timeStart: this.eventTime[0].format("HH:mm:ss"),
+            timeEnd: this.eventTime[1].format("HH:mm:ss"),
+            duration: Number(
+              (this.eventDate[1].diff(this.eventDate[0], "days") + 1) *
+                this.eventTime[1].diff(this.eventTime[0], "hours")
+            ),
+            noOfOpenings: Number(this.noOfOpenings),
+            eventCauses: this.eventCauses,
+            badgeAwarded: this.badgeAwarded,
+            participants: {},
+            orgId: this.$store.state.id,
+            orgName: this.$store.state.details.orgName,
           })
+            .then(() => {
+              this.eventCreateNotification();
+              console.log("Document successfully written!");
+            })
+            .catch((error) => {
+              console.error("Error writing document: ", error);
+            });
         });
-     
-        
+      });
     },
     createEvent() {
       this.createDb();
     },
     previewFile(event) {
-      this.file = event.target.files[0]
-      console.log(this.file)
-    }
+      this.file = event.target.files[0];
+      console.log(this.file);
+    },
   },
   async mounted() {
     var docRef = doc(db, "organisation", this.$store.state.id);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
       this.$store.state.details = docSnap.data();
-      console.log(docSnap.data())
+      console.log(docSnap.data());
       this.rendered = true;
     }
-  }
-}
-
+  },
+};
 </script>
 
 <style scoped>
--- src/components/IndividualEvent.vue
+++ src/components/IndividualEvent.vue
@@ -9,7 +9,7 @@
               <div id="smallHeader">
                 <h1 id="causeTitle">Causes</h1>
                 <div class="box" id="causeContainer">
-                  <div class="causeBox" v-for="type in eventType">
+                  <div class="causeBox" v-for="type in eventType" :key="type">
                     {{ type }}
                   </div>
                 </div>
@@ -60,7 +60,9 @@
                   <p>
                     <b>{{ fullDate }}</b>
                   </p>
-                  <p><b>{{displayTime}}</b></p>
+                  <p>
+                    <b>{{ displayTime }}</b>
+                  </p>
                   <p>
                     <b>at {{ displayLocation }}</b>
                   </p>
@@ -119,7 +121,7 @@
                   ><img src="@/assets/star.svg" />
                   {{ displayExpGain }} exp</span
                 ><br />
-                <p v-for="badge in badgeType">
+                <p v-for="badge in badgeType" :key="badge">
                   {{ badge }} upon successful completion
                 </p>
               </div>
@@ -136,6 +138,9 @@
 import { db } from "../firebase.js";
 import NoPageFound from "@/views/NoPageFound.vue";
 import { doc, setDoc, updateDoc, getDoc } from "firebase/firestore";
+import { notification } from "ant-design-vue";
+import { SmileOutlined, robotOutlined } from "@ant-design/icons-vue";
+import { h } from "vue";
 
 export default {
   name: "IndividualEvent",
@@ -153,6 +158,30 @@ export default {
       visible: false,
     };
   },
+
+  setup() {
+    const successfulEventApplied = () => {
+      notification.open({
+        message: "Success",
+        description:
+          "Succesfully applied for event! Organisation will get back soon.",
+        duration: 3,
+        icon: () => h(SmileOutlined, { style: "color: #020957" }),
+      });
+    };
+
+    const error = () => {
+      notification.open({
+        message: "Error",
+        description: "An Error Occurred. Please try again. ",
+        duration: 3,
+        icon: () => h(robotOutlined, { style: "color: #ff3700" }),
+      });
+    };
+
+    return successfulEventApplied, error;
+  },
+
   computed: {
     fullDate() {
       const monthNames = [
@@ -169,12 +198,12 @@ export default {
         "November",
         "December",
       ];
-      let startDate = this.eventStartDate.split('-');
-      startDate[1] = monthNames[startDate[1]]
-      let endDate = this.eventEndDate.split('-');
-      endDate[1] = monthNames[endDate[1]]
-      startDate = startDate.join(' ')
-      endDate = endDate.join(' ')
+      let startDate = this.eventStartDate.split("-");
+      startDate[1] = monthNames[startDate[1]];
+      let endDate = this.eventEndDate.split("-");
+      endDate[1] = monthNames[endDate[1]];
+      startDate = startDate.join(" ");
+      endDate = endDate.join(" ");
       if (startDate == endDate) {
         return this.startDate;
       } else {
@@ -182,10 +211,10 @@ export default {
       }
     },
     displayExpGain() {
-      let timeStart = this.event["timeStart"].split(':').map(Number);
-      let timeEnd = this.event["timeEnd"].split(':').map(Number);
+      let timeStart = this.event["timeStart"].split(":").map(Number);
+      let timeEnd = this.event["timeEnd"].split(":").map(Number);
       let hh = 0;
-      hh += (timeEnd[0] - timeStart[0])
+      hh += timeEnd[0] - timeStart[0];
       if (hh == 0) {
         return 50;
       } else {
@@ -193,23 +222,33 @@ export default {
       }
     },
     displayTime() {
-      const zeroPad = (num, places) => String(num).padStart(places, '0')
-      let timeStart = this.event["timeStart"].split(':').map(Number)
+      const zeroPad = (num, places) => String(num).padStart(places, "0");
+      let timeStart = this.event["timeStart"].split(":").map(Number);
       if (timeStart[0] >= 12) {
-        timeStart[2] = "pm"
+        timeStart[2] = "pm";
       } else {
-        timeStart[2] = "am"
+        timeStart[2] = "am";
       }
-      let timeEnd = this.event["timeEnd"].split(':').map(Number)
+      let timeEnd = this.event["timeEnd"].split(":").map(Number);
       if (timeEnd[2] >= 12) {
-        timeEnd[2] = 'pm'
+        timeEnd[2] = "pm";
       } else {
-        timeEnd[2] = 'am'
+        timeEnd[2] = "am";
       }
       timeStart[0] = timeStart[0] % 12;
       timeEnd[0] = timeEnd[0] % 12;
-      timeStart = String(timeStart[0]) + "." + String(zeroPad(timeStart[1], 2)) + " " + String(timeStart[2])
-      timeEnd = String(timeEnd[0]) + "." + String(zeroPad(timeEnd[1], 2)) + " " + String(timeEnd[2])  
+      timeStart =
+        String(timeStart[0]) +
+        "." +
+        String(zeroPad(timeStart[1], 2)) +
+        " " +
+        String(timeStart[2]);
+      timeEnd =
+        String(timeEnd[0]) +
+        "." +
+        String(zeroPad(timeEnd[1], 2)) +
+        " " +
+        String(timeEnd[2]);
       return `${timeStart} to ${timeEnd}`;
     },
     displayLocation() {
@@ -275,11 +314,9 @@ export default {
         this.updateEvent();
         this.visible = false;
         this.hasRegistered = true;
-        alert(
-          "Succesfully applied for event! Organisation will get back soon!"
-        );
+        this.successfulEventApplied();
       } catch {
-        alert("Error registering, please try again!");
+        this.error();
       }
     },
     clickVolunteer() {
@@ -300,13 +337,13 @@ h1 {
 }
 #wrapper {
   margin-top: 50px;
-  justify-content: center
+  justify-content: center;
 }
 #content {
   width: 75%;
 }
 #imgDiv {
-  display:block;
+  display: block;
   width: 100%;
   margin-right: 10px;
 }
