cd1ec6bf41222d17a8d4827b4f29adb7e54a62c2
ImmaCrayon81 <e0559120@u.nus.edu>
2022-10-29 18:02:43 +0800

    Added recommended events

--- src/components/volunteer/VolCurrentEvents.vue
+++ src/components/volunteer/VolCurrentEvents.vue
@@ -1,10 +1,11 @@
 <template>
+  <h1 class="header-text">Events For You</h1>
   <div class="blog-cards">
-    <VolEventCard
-      :event="event"
-      v-for="(event, index) in EventCards"
-      :key="index"
-    />
+    <VolEventCard :event="event" v-for="(event, index) in interestCards" :key="index"/>
+  </div>
+  <h1 class="header-text">All Events</h1>
+  <div class="blog-cards">
+    <VolEventCard :event="event" v-for="(event, index) in allCards" :key="index"/>
   </div>
 </template>
 
@@ -19,33 +20,54 @@ export default {
   name: "VolCurrentEvents",
   data() {
     return {
-      EventCards: [],
+      interestCards: [],
+      allCards: []
     };
   },
   components: {
     VolEventCard,
   },
   mounted() {
-    this.queryDb();
+    this.queryInterests();
+    this.queryEvents();
   },
   methods: {
-    async queryDb() {
-      const eventSnapshot = await getDocs(collection(db, "events"));
+    async queryInterests() {
+      const userInterests = this.$store.state.details["interests"]
+      const q = query(collection(db, "events"), where('eventCauses', 'array-contains-any', userInterests))
+      const eventSnapshot = await getDocs(q)
       eventSnapshot.forEach((doc) => {
-        console.log(doc.id, "=>", doc.data());
-        this.EventCards.push({ id: doc.id, data: doc.data() });
-      });
+        console.log(doc.id, "=>", doc.data())
+        this.interestCards.push({ id: doc.id, data: doc.data()})
+      })
     },
+    async queryEvents() {
+      const eventSnapshot = await getDocs(collection(db, "events"))
+      eventSnapshot.forEach((doc) => {
+        console.log(doc.id, "=>", doc.data())
+        this.allCards.push({ id: doc.id, data: doc.data()})
+      })
+    }
   },
 };
 </script>
 
 <style scoped>
 .blog-cards {
-  margin-top: 40px;
+  margin-top: 32px;
   display: flex;
   flex-wrap: wrap;
-  width: 90%;
   margin-right: 0;
 }
+
+.header-text {
+  color: #020957;
+  text-align: left;
+  margin-top: 40px;
+}
+
+.header-text:last-of-type {
+  margin-top: 0;
+}
+
 </style>
