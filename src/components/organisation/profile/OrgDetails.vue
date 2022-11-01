2da962118698f90d73d467ef993ce4617b103795
a.anghs22 <a.anghs22@gmail.com>
2022-11-02 00:49:48 +0800

    added change password functionality

--- src/components/organisation/profile/OrgDetails.vue
+++ src/components/organisation/profile/OrgDetails.vue
@@ -9,59 +9,88 @@
       </div>
 
       <div class="box button">
-        <div class="ant-button">
-          <a-button
-            type="primary"
-            size="large"
-            class="orange"
-            @click="showModal"
-          >
-            Change Password
-          </a-button>
-          <!-- <a-modal
-                v-model:visible="visible"
-                title="Event Confirmation"
-                @ok="handleOk"
-              >
-                <template #footer> </template>
-                <div class="modal">
-                  <h1 id="modalHeader">
-                    Hi, {{ `${this.$store.state.details["lastName"]}!` }}
-                  </h1>
-                  <p>Yes, I want to sign up as a volunteer for</p>
-                  <p>
-                    <b
-                      >{{ this.event["eventName"] }} by
-                      {{ this.event["eventHost"] }}</b
-                    >
-                  </p>
-                  <p>on</p>
-                  <p>
-                    <b>{{ fullDate }}</b>
-                  </p>
-                  <p><b>TBC</b></p>
-                  <p>
-                    <b>at {{ displayLocation }}</b>
-                  </p>
-                  <br /><br />
-                  <span id="spanModal">
-                    <p>I agree with the</p>
-                    <p style="color: orange">Terms & Conditions</p>
-                  </span>
-
-                  <a-button id="confirmButton" htmlType="submit" size="large" type="primary" danger @click="confirmVolunteer"
-                    >Confirm
-                  </a-button>
-                </div>
-              </a-modal> -->
+            <div class="ant-button">
+            <a-button type="primary" size="large" class="orange" @click="edit_details">
+                Change Password </a-button>
         </div>
-      </div>
+
+        <div class="a-modal">
+            <a-modal
+            v-model:visible="this.canEdit"
+            title="Change Password:"
+            @ok="handleOk"
+            >
+
+            <template #footer>
+              <div class="ant-button">
+                <a-button key="cancel" @click="confirmCancel" style="width: 40%">
+                  Cancel</a-button>
+                <a-button
+                    class="orange"
+                    key="Confirm"
+                    type="primary"
+                    :loading="loading"
+                    @click="changeDetails"
+                    style="width: 40%"
+                    :disabled="this.confirmDetails == false"
+                    >Confirm</a-button
+                >
+              </div>
+            </template>
+            
+            <div class="modalContent">
+                <a-form
+                    id="editDetails"
+                    class="edit-details-form"
+                    ref="editDetails"
+                    @submit.prevent="confirmDetails"
+                    >
+                    <a-form-item class="form">
+                        <label class="formSignUp">Current Password</label><br />
+                        <a-input
+                        style="width: 60%; margin-bottom: 10px"
+                        class="input"
+                        type="email"
+                        v-model:value="currentPassword"
+                        placeholder="Enter your current password"
+                        ></a-input> <br />
+
+                        <label class="formSignUp">New Password</label><br />
+                        <a-input
+                        style="width: 60%; margin-bottom: 10px"
+                        class="input"
+                        type="email"
+                        v-model:value="newPassword"
+                        placeholder="Enter your new password"
+                        ></a-input>
+
+                    </a-form-item> 
+                </a-form>
+                <div class="confirmBox">
+                    <a-checkbox
+                        class="a-checkbox"
+                        v-model:checked="this.confirmDetails"
+                        ><h4 style="text-align: center">
+                        My details are correct.
+                        </h4></a-checkbox
+                    >
+                </div>
+            </div>
+            </a-modal>
+          </div>         
     </div>
   </div>
+</div>
 </template>
   
 <script>
-import { updateEmail } from "firebase/auth";
+import { updatePassword, getAuth, EmailAuthProvider, reauthenticateWithCredential} from "firebase/auth";
+import { notification } from "ant-design-vue";
+import {
+  SmileOutlined,
+  RobotOutlined,
+} from "@ant-design/icons-vue";
+import { defineComponent, h } from "vue";
 
 export default {
   name: "ContactDetails",
@@ -69,12 +98,95 @@ export default {
     return {
       name: this.$store.state.details["orgName"],
       email: this.$store.getters.getEmail,
+      newPassword: '',
+      currentPassword: '',
+      canEdit: false,
+      seenAll: false,
+      confirmDetails: false, 
     };
   },
+  setup() {
+    const success = () => {
+      notification.open({
+        message: "Success",
+        description: "Personal Details Successfully Updated!",
+        duration: 3,
+        icon: () => h(SmileOutlined, { style: "color: #020957" }),
+      });
+    };
+
+    const error = (msg) => {
+      notification.open({
+        message: "Error",
+        description: msg,
+        duration: 3,
+        icon: () => h(RobotOutlined, { style: "color: #ff3700" }),
+      });
+    };
+
+    return {
+      success,
+      error
+    }
+  },
   methods: {
-    edit_profile() {
-      this.$store.dispatch("updateProfile");
+    edit_details() {
+      this.canEdit = true;
     },
+    showNone() {
+      this.seenAll = false; 
+      this.canEdit = false;
+    },
+    confirmCancel() {
+      this.canEdit = false;
+      this.confirmDetails = false;
+      location.reload();
+    },
+    confirm() {
+      this.confirmDetails = true;
+    },
+    changePassword(currentPassword, newPassword) {
+      const auth = getAuth();
+      const user = auth.currentUser;
+
+      updatePassword(user, newPassword).then(() => {
+        // need to commit change to user!!!!
+        this.$store.commit("updateOrganisation", user);
+        console.log('password changed');
+      }).catch((error) => {
+        const errorMessage = error.message;
+        this.error('Unable to change password, please try again! \n' + errorMessage);
+        console.log(errorMessage);
+
+        // reauthentication without relogin from user
+        const credential = EmailAuthProvider.credential(
+          user.email, currentPassword
+        );
+        reauthenticateWithCredential(user, credential);
+        updatePassword(user, newPassword);
+
+
+      })
+
+    },
+    async changeDetails() {
+      // check that fields were updated:
+      if (this.currentPassword != "" & this.newPassword != "") {
+        // Pop Data from FS
+        console.log(this.currentPassword, this.newPassword);
+        this.confirmDetails = false;
+        this.canEdit = false;
+      }
+
+      // logging changes
+      console.log('updating password...');
+      await this.changePassword(this.currentPassword, this.newPassword);
+
+      // success confirmation 
+      this.success();
+      // location.reload();
+    }
+
   },
 };
 </script>
