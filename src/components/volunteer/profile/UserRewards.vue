a661e7f6bcb6b5052c442a863db35b661069fd03
Zhou Wai <wongzhouwai@gmail.com>
2022-11-01 15:08:45 +0800

    User Rewards Stash

--- src/components/volunteer/profile/UserRewards.vue
+++ src/components/volunteer/profile/UserRewards.vue
@@ -3,12 +3,59 @@
     <div class="box">
       <div id="box-title">Rewards</div>
     </div>
+    <div class="box rewardsDisplay">
+      <div class="singleReward" v-if="rewardArray.length == 1"></div>
+      <p>{{ this.rewardArray }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import { collection, query, where } from "firebase/firestore";
+import { db } from "../../../firebase.js";
+import { getDoc, doc } from "firebase/firestore";
 export default {
   name: "UserRewards",
+  data() {
+    return {
+      rewardArray: [],
+    };
+  },
+  mounted() {
+    this.getRewards();
+  },
+  methods: {
+    async getRewards() {
+      const userRewards = this.$store.state.details["userRewards"];
+      console.log(userRewards);
+      for (const rewardLevel of Object.keys(userRewards)) {
+        var rewardId = userRewards[rewardLevel]["id"];
+        console.log(rewardId);
+        if (rewardId != "") {
+          var rewardDetails = this.getRewardDetails(rewardId);
+          console.log(rewardDetails.data());
+
+          if (rewardDetails != {}) {
+            var rewardDisplayed = [
+              rewardDetails["merchant"],
+              rewardDetails["name"],
+              rewardId,
+            ];
+            this.rewardArray.push(rewardDisplayed);
+            console.log(this.rewardArray);
+          } else {
+            console.log("No such Document");
+          }
+        }
+      }
+    },
+
+    async getRewardDetails(rewardId) {
+      const docRef = doc(db, "level", rewardId);
+      const docSnap = await getDoc(docRef);
+      return docSnap.data();
+    },
+  },
 };
 </script>
 
@@ -24,7 +71,7 @@ export default {
 
 .box {
   text-align: left;
-  margin-left: 15px;
+  padding-left: 15px;
 }
 
 #box-title {
