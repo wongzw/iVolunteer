cbf0672629cb76fb6ee3977f5d14fb35c405b04f
Zhou Wai <wongzhouwai@gmail.com>
2022-11-03 15:39:28 +0800

    Working Notificaiton

--- src/views/volunteers/VolunteerNotification.vue
+++ src/views/volunteers/VolunteerNotification.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import UserNotification from "../../components/volunteer/UserNotification.vue";
+import UserNotification from "../../components/volunteer/notifications/UserNotification.vue";
 
 export default {
   name: "UserNotifications",
