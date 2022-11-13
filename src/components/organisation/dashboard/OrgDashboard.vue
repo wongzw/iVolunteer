4f21e27c2187df4c2c4437fa5cdd0464fc892901
Zhou Wai <wongzhouwai@gmail.com>
2022-11-14 01:42:56 +0800

    Update Org Dashboard Current Events

--- src/components/organisation/dashboard/OrgDashboard.vue
+++ src/components/organisation/dashboard/OrgDashboard.vue
@@ -98,7 +98,19 @@ export default {
         eventSnapshot.forEach((doc) => {
           if (doc.id == ev) {
             console.log(doc.id, "=>", doc.data());
-            this.allCards.push({ id: ev, data: doc.data() });
+            const str = doc.data().dateEnd;
+
+            // parse string & make into datetime object
+            const [day, month, year] = str.split("-");
+            const date = new Date(year, month - 1, day);
+
+            // get today's date
+            const today = new Date(); // returns time
+
+            // only show the events that have yet to end; compare time
+            if (date.getTime() >= today) {
+              this.allCards.push({ id: ev, data: doc.data() });
+            }
           }
         });
       });
